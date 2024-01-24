edef54ea246108994c864ad9fb8576e0eba6e435
Drischdaan <drischdaan@gmail.com>
2024-01-24 11:13:41 +0100

    feat: adds tan stack query

---
+++ src/components/providers/Providers.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import { TanStackQueryProvider } from "./TanStackQueryProvider";
+
+export interface IProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: IProvidersProps) {
+  return (
+    <>
+      <TanStackQueryProvider>{children}</TanStackQueryProvider>
+    </>
+  );
+}
---
+++ src/components/providers/TanStackQueryProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+export interface IQueryProviderProps {
+  children: ReactNode;
+}
+
+export function TanStackQueryProvider({ children }: IQueryProviderProps) {
+  const [client] = useState(() => new QueryClient());
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
